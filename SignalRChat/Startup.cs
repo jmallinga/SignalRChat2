aea301927928b94f12ae2499fe83c8918b4ac5e9
jmallinga <jmallinga@gmail.com>
2015-05-05 09:15:32 -0400

    Moved Files Over

---
+++ SignalRChat/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(SignalRChat.Startup))]
+
+namespace SignalRChat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
